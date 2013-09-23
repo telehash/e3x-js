29e8b366fb95dfeb3d6f2375105328e4a05ff175
Jeremie Miller <jeremie@jabber.org>
2013-09-23 10:56:17 -0600

    nodesocket now works as a bridge

--- nodesocket/socketd.js
+++ nodesocket/socketd.js
@@ -1,23 +1,7 @@
 var dgram = require("dgram");
-// UDP Server
-var server = dgram.createSocket("udp4");
-var address;
 
-server.on("error", function (err) {
-  console.log("server error:\n" + err.stack);
-  server.close();
-});
 
-server.on("listening", function () {
-  address = server.address();
-  console.log("server listening " +
-      address.address + ":" + address.port);
-});
 
-server.bind(8910, function() {});
-
-// UDP Client
-var client = dgram.createSocket("udp4");
 
 var httpServer = require("http").createServer(function(req, resp) {
   req.url.replace("..",""); // this is super dumb minimal stub, don't actually use as a server
@@ -27,31 +11,36 @@ var httpServer = require("http").createServer(function(req, resp) {
         resp.writeHeader(200, {"Content-Type": "text/html"});
         resp.end(content);
     });
-}).listen(process.env.PORT || 8088);
+}).listen(process.env.PORT || 8008);
 
 var clients = [];
 var io = require("socket.io").listen(httpServer);
 io.sockets.on("connection", function (socket) {
-  console.log("connected");
-  socket.emit('connected', { ip: address.address, port: address.port });
-  
-  socket.on("listen", function(callback) {
-    callback({data:(new Buffer("foo")).toString("base64")});
+  console.log("starting");  
+  var server = dgram.createSocket("udp4");
+  server.on("error", function (err) {
+    console.log("server error:\n" + err.stack);
+    server.close();
   });
-  
+  server.on("listening", function() {
+    console.log("connected");
+    socket.emit('connected', {});
+  });
+  server.on("message", function (msg, rinfo) {
+    console.log("server got: " + msg + " from " + rinfo.address + ":" + rinfo.port)
+    socket.emit("message", {message: msg.toString("base64"), ip: rinfo.address, port: rinfo.port});
+  });
+  server.bind();
   socket.on("message", function(data) {
-    console.log("Received data from socket.io: " + data);
+    console.log("Received data from socket.io: ",data);
+    if(!data.message) return;
     packet = new Buffer(data.message, "base64");
-    client.send(packet, 0, packet.length, data.port, data.ip, function(err, bytes) {
+    if(!packet || !packet.length) return;
+    server.send(packet, 0, packet.length, data.port, data.ip, function(err, bytes) {
       console.log("Sent following msg to " + data.ip + ":" + data.port + " - " + data.message);
-      client.close();
     });
   });
   
-  server.on("message", function (msg, rinfo) {
-    console.log("server got: " + msg + " from " + rinfo.address + ":" + rinfo.port)
-    socket.emit("message", {message: msg, ip: rinfo.address, port: rinfo.port});
-  });
 });
 
 
