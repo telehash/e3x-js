95dd844d0a5b86323252c138df1badc67852bf0f
Jeremie Miller <jeremie@jabber.org>
2013-11-11 13:31:28 -0700

    updated readme, and adding package stuff for node-telehash to use thjs

--- README.md
+++ README.md
@@ -6,4 +6,8 @@ Since telehash requires a real udp socket, there are various ways to use thjs in
 * nodesocket - a minimal node.js service that bridges raw udp sockets to any browser over socket.io
 * chromeapp - bindings to use the udp interface available to chrome apps
 
+If you're looking to use this in node, check out the [node-telehash](https://github.com/telehash/node-telehash) package (in npm as just `telehash`).
+
+To get a simple demo running using nodesocket as the backend, first run `npm install` to install the dependencies, then in the nodesocket directory run `node socketd.js` and in your browser go to http://localhost:8008/test/groupchat.html.  It should first generate a key (one time only) and then let you choose a nickname to create or join a rudimentary chatroom demo.  Use two different browsers to join/test multiple times.
+
 The crypto that powers this is only possible thanks to the incredible work done by the team behind [Forge](https://github.com/digitalbazaar/forge) and [Tom Wu](http://www-cs-students.stanford.edu/~tjw/).
--- seeds.js
+++ seeds.js
@@ -3,7 +3,8 @@ var seeds = [{
     "port": 42424,
     "pubkey": "-----BEGIN PUBLIC KEY-----\nMIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAxoQkh8uIPe18Ym5kO3VX\nqPhKsc7vhrMMH8HgUO3tSZeIcowHxZe+omFadTvquW4az7CV/+3EBVHWzuX90Vof\nsDsgbPXhzeV/TPOgrwz9B6AgEAq+UZ+cs5BSjZXXQgFrTHzEy9uboio+StBt3nB9\npLi/LlB0YNIoEk83neX++6dN63C3mSa55P8r4FvCWUXue2ZWfT6qamSGQeOPIUBo\n4aiN6P4Hzqaco6YRO9v901jV+nq0qp0yHKnxlIYgiY7501vXWceMtnqcEkgzX4Rr\n7nIoA6QnlUMkTUDP7N3ariNSwl8OL1ZjsFJz7XjfIJMQ+9kd1nNJ3sb4o3jOWCzj\nXwIDAQAB\n-----END PUBLIC KEY-----\n"}];
 
-seeds[0] = 	{
+//seeds[0] = 	{
+var x = 	{
 	    "ip": "localhost",
 	    "port": 42424,
 	    "pubkey": "-----BEGIN PUBLIC KEY-----\nMIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAwpYq+d5qkc3ReQ0To9t6\n9mRj5O5zR2pje0vJB9Cs7+tRAVfRcQx3NW/NL4BdgNRZiekf1rqnEsjy9QceEqCK\nrJJeyLnel6pQAb9zJQ9J1HCYIg/NpMJz+U7Wiwpmxd9uGbOY0GloGVCcO/DeAQVS\ne6l79Y3teiCF5qsM5FW/wSDnA87rK6HdNIKU+gsUgc+9kcywQ1T/rLyG4BRZ168E\npTJMsgRY0uUizCtzvoqV8y0ZE1+1x1oJ3EmgFq7HK258C4vhP/AJYFl9mB5WYYpo\nLz3dSGnnLwn0naU1m6THl5CJVmxf/6JPj7AeGme2KmAdVtaH4WImYg2TwCeIuGQo\nkwIDAQAB\n-----END PUBLIC KEY-----\n"}
