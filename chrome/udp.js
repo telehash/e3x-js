072c2ed33b2aac6d0cf02e7c9be2ca9d59721a07
Jeremie Miller <jeremie@jabber.org>
2013-11-20 00:35:01 -0700

    skeleton to start the chrome app example

---
+++ chrome/udp.js
@@ -0,0 +1,5 @@
+//chrome.socket.create(SocketType type, object options, function callback)
+//chrome.socket.bind(integer socketId, string address, integer port, function callback)
+//chrome.socket.recvFrom(integer socketId, integer bufferSize, function callback)
+//chrome.socket.sendTo(integer socketId, arraybuffer data, string address, integer port, function callback)
+
