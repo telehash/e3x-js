0e74d36f21ca270aeecbe59d48d657868e9982dd
Jeremie Miller <jeremie@jabber.org>
2014-08-24 19:58:50 -0600

    moving things around a bit to prep for conversion

--- test/e3x.test.js
+++ test/e3x.test.js
@@ -6,6 +6,10 @@ describe('e3x', function(){
 
   it('should export an object', function(){
     expect(e3x).to.be.a('object');
-  })
+  });
+
+  it('should have cipher sets loaded', function(){
+    expect(Object.keys(e3x.cs).length).to.be.equal(1);
+  });
 
 });
