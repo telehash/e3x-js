35594682160ab775af31ccb780a6bacf4c2109d3
Ryan Bennett <nomad.ry@gmail.com>
2015-06-16 14:05:42 -0600

    require cs2a from local ciphers

--- index.js
+++ index.js
@@ -3,5 +3,5 @@ exports = module.exports = require('./e3x.js');
 
 // load supported ciphersets
 exports.cs['1a'] = require('./ciphers/1a/export.js');
-exports.cs['2a'] = require('e3x-cs2a');
+exports.cs['2a'] = require('./ciphers/2a/export.js');
 exports.cs['3a'] = require('e3x-cs3a');
