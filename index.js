5f345087560cf0922edd5aca4ebe83acba799997
Ryan Bennett <nomad.ry@gmail.com>
2015-06-16 16:59:25 -0600

    all ciphers local

--- index.js
+++ index.js
@@ -4,4 +4,4 @@ exports = module.exports = require('./e3x.js');
 // load supported ciphersets
 exports.cs['1a'] = require('./ciphers/1a/export.js');
 exports.cs['2a'] = require('./ciphers/2a/export.js');
-exports.cs['3a'] = require('e3x-cs3a');
+exports.cs['3a'] = require('./ciphers/3a/export.js');
