a1b7aec1fb05785b3e416b6c6eea3f63a8ea6b7f
Ryan Bennett <nomad.ry@gmail.com>
2015-06-22 14:29:44 -0600

    add unitTests and srcfiles to watching mocha and chext

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,14 @@
 
 var gulp = require('gulp');
 var mocha = require('gulp-mocha');
+var watch = require('gulp-watch')
+var chext = require("chext")
+
+var unitTests =  ['test/cs1a.test.js', 'test/cs2a.test.js', 'test/cs3a.test.js', 'test/e3x.test.js'];
+var srcFiles = ['ciphers/**/*.js', "e3x.js"]
 
 gulp.task('mocha', function() {
-  return  gulp.src(['test/cs1a.test.js', 'test/cs2a.test.js', 'test/cs3a.test.js', 'test/e3x.test.js'], { read: false })
+  return  gulp.src( unitTests, { read: false })
               .pipe(mocha({ reporter: 'list' }))
               .on('error', function(){
               })
@@ -13,5 +18,13 @@ gulp.task('mocha', function() {
 });
 
 gulp.task('dev', ['mocha'], function() {
+  var ch = new chext()
 
+  ch.watchify(unitTests)
+
+  ch.on("testing_complete",function(results){
+    console.log("tests complete", results)
+  })
+
+  gulp.watch(unitTests.concat(srcFiles), ["mocha"])
 })
