ff77c641c3984babf111cea31bed96a7acc84463
Ryan Bennett <nomad.ry@gmail.com>
2015-07-11 22:49:06 -0600

    add gulp plato and jsdoc

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,9 @@
 var gulp = require('gulp');
 var mocha = require('gulp-mocha');
 var watch = require('gulp-watch')
-var chext = require("chext")
+var chext = require("chext");
+var jsdoc = require("gulp-jsdoc");
+var glob  = require("glob");
 
 var unitTests =  ['test/cs1a.test.js', 'test/cs2a.test.js', 'test/cs3a.test.js', 'test/e3x.test.js'];
 var srcFiles = ['ciphers/**/*.js', "e3x.js"]
@@ -16,6 +18,30 @@ gulp.task('mocha', function() {
 
               });
 });
+
+var plato = require('plato');
+
+var files = glob.sync("ciphers/*/*.js").concat(['e3x.js']);
+
+var outputDir = './plato';
+// null options for this example
+var options = {
+  title: 'Your title here'
+};
+
+var callback = function (report){
+// once done the analysis,
+// execute this
+};
+
+
+gulp.task('doc', function(){
+
+  plato.inspect(files, outputDir, {}, callback);
+  gulp.src(["./lib/*.js", "./ext/*.js"])
+  .pipe(jsdoc('./doc'))
+})
+
 
 gulp.task('dev', function() {
   var ch = new chext()
